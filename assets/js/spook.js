260cb093dbce63aa29b573c4983ff577d43b26be
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-11-22 06:29:36 -0500

    SPOOKY!!!!!

--- assets/js/spook.js
+++ assets/js/spook.js
@@ -1,4 +1,4 @@
-var spookySound = ("/assets/sound/spooky.mp3")
+var spookySound = new Audio("assets/sound/spooky.mp3")
 var button = document.getElementById("button")
 button.addEventListener("click", function(){
   spookySound.play()
