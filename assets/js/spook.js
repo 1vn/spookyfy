a812582d4d978d51764880fd88603d3c17e79040
Qasim Iqbal <me@qas.im>
2014-11-23 00:47:43 -0500

    efficiency more spooked

--- assets/js/spook.js
+++ assets/js/spook.js
@@ -1,9 +1,9 @@
+document.body.innerHTML +=
+      '<audio id="sp00k" src="http://qas.im/projects/spookyfy/spooky.mp3" preload="auto"></audio>'
 document.body.addEventListener("click", function() {
   var rand = Math.floor(Math.random() * 100);
   console.log(rand);
   if (rand % 5 == 0) {
-    document.body.innerHTML +=
-      '<audio id="sp00k" src="http://qas.im/projects/spookyfy/spooky.mp3" preload="auto"></audio>'
     document.getElementById('sp00k').play();
   }
 })
