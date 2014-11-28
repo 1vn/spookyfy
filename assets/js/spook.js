798dbcf495d00d917719951ccca54cc04a02972f
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2014-11-28 14:57:18 -0500

    Laid the foundation for MY BOY qasim spook

--- assets/js/spook.js
+++ assets/js/spook.js
@@ -1,9 +1,15 @@
 document.body.innerHTML +=
-      '<audio id="sp00k" src="http://qas.im/projects/spookyfy/spooky.mp3" preload="auto"></audio>'
-document.body.addEventListener("click", function() {
+  '<audio id="sp00k" src="http://qas.im/projects/spookyfy/spooky.mp3" preload="auto"></audio>'
+document.body.addEventListener("click", function(event) {
   var rand = Math.floor(Math.random() * 100);
   console.log(rand);
-  if (rand % 5 == 0) {
+  if (rand % 10 == ) {
     document.getElementById('sp00k').play();
   }
+  if (rand == 1) {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+    document.body.innerHTML += '<div id="sp00kdiv">foo</div>'
+  }
+
 })
