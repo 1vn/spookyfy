24984800dd41dfb5ffd54f57fc6f4492257cc970
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-04-28 20:59:23 -0400

    yolo

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,3 @@
 var totalSpooks = 0
-console.log("LOL")
 chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-chrome.browserAction.setBadgeText({text: totalSpooks}, function(){
-	console.log("Success!")
-})
+chrome.browserAction.setBadgeText({'text': + "0"})
