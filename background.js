911379835b8d2d4dd0a4d79750ba58ed3287beed
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-05-02 23:25:12 -0400

    naming

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 console.log("Background loaded...")
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
 	chrome.browserAction.setBadgeBackgroundColor({ color: [255, 255, 255, 255] })
-	chrome.browserAction.setBadgeText({'text': request.counter})
+	chrome.browserAction.setBadgeText({'text': request.totalSpooks})
 })
