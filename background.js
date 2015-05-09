7345c672c47c846e29e44aa02a9fadabb66b0b35
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-05-08 23:43:29 -0400

    desperation

--- background.js
+++ background.js
@@ -1,13 +1,11 @@
-console.log("Background loaded...")
+
 var totalSpooks;
 chrome.storage.sync.get('totalSpooks', function(item){
-	console.log(item.totalSpooks)
 	totalSpooks=item.totalSpooks
 	if(!totalSpooks){totalSpooks=0}
 })
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
-	console.log("Adding to counter...")
 	totalSpooks++
 	chrome.browserAction.setBadgeBackgroundColor({ color: [255, 255, 255, 255] })
 	chrome.browserAction.setBadgeText({'text': totalSpooks.toString()})
@@ -15,7 +13,6 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
 
 chrome.windows.onRemoved.addListener(function(windowId){
 	chrome.storage.sync.set({'totalSpooks': totalSpooks}, function(){
-	console.log("Spookysaved")
 	})
 })
 
