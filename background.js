01b23bfc84b605e3eb247301188ebe469ca9cc77
ivanzhangsolutions <ivanzhangsolutions@gmail.com>
2015-05-12 00:21:29 -0400

    yolo

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 
 var totalSpooks;
+var superSpooks;
 chrome.storage.sync.get('totalSpooks', function(item){
 	totalSpooks=item.totalSpooks
 	if(!totalSpooks){totalSpooks=0}
+	if(!superSpooks){superSpooks=0}
 })
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
@@ -10,6 +12,9 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
 		totalSpooks++
 		chrome.browserAction.setBadgeBackgroundColor({ color: [255, 255, 255, 255] })
 		chrome.browserAction.setBadgeText({'text': totalSpooks.toString()})
+	}
+	if(request.action == "superspooked"){
+		superSpooks++;
 	}
 })
 
